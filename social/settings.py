446f025a720bcbaf65ec0c96c1dd8fc3f058f01d
Emily Traynor <72641674+emily0622@users.noreply.github.com>
2023-03-20 16:06:25 -0400

    changed debug to false

--- social/settings.py
+++ social/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0d&@&#$+iplcor#dzu50(oe3d#e$bakyoe@5!ydmteqk%&%w9l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['ventnet.herokuapp.com']
 
