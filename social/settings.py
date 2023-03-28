b24f1067b4f886f48cb1986ad841fb11d4902efe
Emily Traynor <emily.traynor@mail.utoronto.ca>
2023-03-27 22:10:06 -0400

    moved secret key

--- social/settings.py
+++ social/settings.py
@@ -18,7 +18,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY='django-insecure-0d&@&#$+iplcor#dzu50(oe3d#e$bakyoe@5!ydmteqk%&%w9l'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
